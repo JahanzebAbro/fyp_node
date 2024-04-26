b415cd88f8d6e59a07753549013778f8a9953fad
JahanzebAbro <jahanzebabro2001@gmail.com>
2024-04-26 21:00:16 +0100

    Completed view chart and filter buttons

--- src/public/scripts/analytics_seeker_utils.js
+++ src/public/scripts/analytics_seeker_utils.js
@@ -1,3 +1,5 @@
+let job_views_chart = '';
+let app_rate_chart = '';
 
 // APPLICATION RATE CHART
 $(document).ready(function(){
@@ -5,9 +7,9 @@ $(document).ready(function(){
     const ctx = $('#app_rate_chart');
     const data = JSON.parse(ctx.attr('data-application-rate'));
 
-    const dates = data.map(date => date.date);
+    const dates = data.map(date => new Date(date.date));
     const counts = data.map(count => count.count);
-
+    const curr_date = new Date();
 
     const chart_data = {
     labels: dates,
@@ -15,6 +17,7 @@ $(document).ready(function(){
         label: 'Application Rate',
         data: counts,
         fill: false,
+        backgroundColor: 'rgb(75, 192, 192)',
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
     }]
@@ -28,11 +31,17 @@ $(document).ready(function(){
                 x: {
                     type: 'time',
                     time: {
+                        unit: 'day',
                         displayFormats: {
-                            day: 'MMM d'
+                            day: 'MMM d yyyy',
+                            week: 'MMM d yyyy',
+                            month: 'MMM yyyy',
+                            year: 'yyyy'
                         },
                         tooltipFormat: 'MMM d, yyyy'
-                    }
+                    },
+                    min: new Date(dates[0].getFullYear(), dates[0].getMonth(), 1),
+                    max: new Date(curr_date.getFullYear(), curr_date.getMonth() + 1, 1)
                 },
 
                 y: {
@@ -45,12 +54,56 @@ $(document).ready(function(){
         }
     };
 
-    const chart = new Chart(ctx, config);
+    app_rate_chart = new Chart(ctx, config);
 
 });
 
 
+// APPLICATION RATE FILTERS
+$(document).ready(function(){
 
+    $('.app_rate_filters').click(function(){
+
+        const filter_type = $(this).attr('data-filter-type');
+
+        const data = JSON.parse($('#app_rate_chart').attr('data-application-rate'));
+        const dates = data.map(date => new Date(date.view_date));
+        
+        let min_date;
+        let max_date;
+        const curr_date = new Date();
+
+        if (filter_type === 'day') {
+
+            min_date = new Date(dates[0].getFullYear(), dates[0].getMonth(), 1);
+            max_date = new Date(curr_date.getFullYear(), curr_date.getMonth() + 1, 1);
+
+        } else if (filter_type === 'week') {
+
+            min_date = new Date(dates[0].getFullYear(), dates[0].getMonth(), dates[0].getDate() - 1);
+            max_date = new Date(curr_date.getFullYear(), curr_date.getMonth(), curr_date.getDate() + 7);
+
+        } else if (filter_type === 'month') {
+
+            min_date = new Date(dates[0].getFullYear(), dates[0].getMonth(), 1);
+            max_date = new Date(curr_date.getFullYear(), curr_date.getMonth() + 1, 1);
+
+        } else if (filter_type === 'year') {
+
+            min_date = new Date(dates[0].getFullYear(), 0, 1);
+            max_date = new Date(curr_date.getFullYear(), 11, 0);
+
+        }
+
+
+        app_rate_chart.config.options.scales.x.time.unit = filter_type;
+        app_rate_chart.config.options.scales.x.min = min_date;
+        app_rate_chart.config.options.scales.x.max = max_date;
+        app_rate_chart.update();
+
+    });
+
+});
 
 // APPLICATION STATUS COUNT CHART
 $(document).ready(function(){
@@ -224,9 +277,9 @@ $(document).ready(function(){
     const ctx = $('#job_views_chart');
     const data = JSON.parse(ctx.attr('data-job-views'));
 
-    const dates = data.map(date => date.view_date);
+    const dates = data.map(date => new Date(date.view_date));
     const counts = data.map(count => count.view_count);
-
+    const curr_date = new Date();
 
     const chart_data = {
     labels: dates,
@@ -245,12 +298,21 @@ $(document).ready(function(){
             scales: {
                 x: {
                     type: 'time',
+                    grid: {
+                        offset: false
+                    },
                     time: {
+                        unit: 'day',
                         displayFormats: {
-                            day: 'MMM d'
+                            day: 'MMM d yyyy',
+                            week: 'MMM d yyyy',
+                            month: 'MMM yyyy',
+                            year: 'yyyy'
                         },
-                        tooltipFormat: 'MMM d, yyyy'
-                    }
+                        tooltipFormat: 'MMM d, yyyy',
+                    },
+                    min: new Date(dates[0].getFullYear(), dates[0].getMonth(), 1),
+                    max: new Date(curr_date.getFullYear(), curr_date.getMonth() + 1, 1)
                 },
 
                 y: {
@@ -263,6 +325,53 @@ $(document).ready(function(){
         }
     };
 
-    const chart = new Chart(ctx, config);
+    job_views_chart = new Chart(ctx, config);
+
+});
+
+
+// JOB VIEWS FILTERS
+$(document).ready(function(){
+
+    $('.job_views_filters').click(function(){
+
+        const filter_type = $(this).attr('data-filter-type');
+        
+        const data = JSON.parse($('#job_views_chart').attr('data-job-views'));
+        const dates = data.map(date => new Date(date.view_date));
+        
+        let min_date;
+        let max_date;
+        const curr_date = new Date();
+
+        if (filter_type === 'day') {
+
+            min_date = new Date(dates[0].getFullYear(), dates[0].getMonth(), 1);
+            max_date = new Date(curr_date.getFullYear(), curr_date.getMonth() + 1, 1);
+
+        } else if (filter_type === 'week') {
+
+            min_date = new Date(dates[0].getFullYear(), dates[0].getMonth(), dates[0].getDate() - 1);
+            max_date = new Date(curr_date.getFullYear(), curr_date.getMonth(), curr_date.getDate() + 7);
+
+        } else if (filter_type === 'month') {
+
+            min_date = new Date(dates[0].getFullYear(), dates[0].getMonth(), 1);
+            max_date = new Date(curr_date.getFullYear(), curr_date.getMonth() + 1, 1);
+
+        } else if (filter_type === 'year') {
+
+            min_date = new Date(dates[0].getFullYear(), 0, 0);
+            max_date = new Date(curr_date.getFullYear(), 11, 0);
+
+        }
+
+
+        job_views_chart.config.options.scales.x.time.unit = filter_type;
+        job_views_chart.config.options.scales.x.min = min_date;
+        job_views_chart.config.options.scales.x.max = max_date;
+        job_views_chart.update();
+
+    });
 
 });
